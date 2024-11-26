28cdc53e3657f147cbe9f6dfa98e4c05045053fc
Sam-Freitas <samfreitas@arizona.edu>
2024-11-26 13:47:08 -0700

    metadata update 2

--- camera_config_and_run2.py
+++ camera_config_and_run2.py
@@ -7,6 +7,7 @@ from PIL import Image, PngImagePlugin
 import numpy as np
 from picamera2 import Picamera2, Preview
 from pprint import pprint
+import matplotlib.pyplot as plt
 
 def save_image_with_metadata(array, filepath, metadata=None, capture_config=None, format="PNG", quality=95):
     if isinstance(array, np.ndarray):
@@ -48,27 +49,22 @@ def save_image_with_metadata(array, filepath, metadata=None, capture_config=None
     else:
         raise ValueError("Unsupported format. Use 'PNG' or 'JPEG'.")
 
-def export_images(arrays, capture_config, image_metadata, camera_metadata, output_path):
+def export_images(arrays, capture_config, image_metadata, camera_metadata, output_path,name_append = ""):
     print("exporting")
-    
-    # Clean the output folder
-    files = glob.glob(os.path.join(output_path, "*"))
-    for f in files:
-        os.remove(f)
 
     # Save metadata
-    with open(os.path.join(output_path, "image_metadata.txt"), "w") as fp:
+    with open(os.path.join(output_path, "image_metadata" + name_append + ".txt"), "w") as fp:
         json.dump(image_metadata, fp)
         # Save metadata
-    with open(os.path.join(output_path, "camera_metadata.txt"), "w") as fp:
+    with open(os.path.join(output_path, "camera_metadata" + name_append + ".txt"), "w") as fp:
         json.dump(camera_metadata, fp)
 
 #  (arrays[0], os.path.join(output_path, "image.png"), metadata, [capture_config, "main"], "PNG"),
 
     # Define image-saving tasks
     tasks = [
-        (arrays[0], os.path.join(output_path, "image.jpg"), image_metadata, [capture_config, "main"], "JPG", 95),
-        (arrays[1], os.path.join(output_path, "lores.jpg"), image_metadata, [capture_config, "lores"], "JPG", 95),
+        (arrays[0], os.path.join(output_path, "image" + name_append + ".png"), image_metadata, [capture_config, "main"], "PNG"),#"JPG", 95),
+        (arrays[1], os.path.join(output_path, "lores" + name_append + ".jpg"), image_metadata, [capture_config, "lores"], "JPG", 95),
     ]
 
     # Use ThreadPoolExecutor for parallel saving
@@ -79,16 +75,26 @@ def export_images(arrays, capture_config, image_metadata, camera_metadata, outpu
         executor.map(save_task, tasks)
 
     print("done exporting")
-        
+
+def sort_dict(d):
+    d_keys = list(d.keys())
+    d_keys.sort()
+    d = {i: d[i] for i in d_keys}
+    return d
+
+def s(tmp):
+    plt.imshow(tmp)
+    plt.show(block = True)
 
 output_path = os.path.join(os.path.dirname(os.path.realpath(__name__)),'images')
 os.makedirs(output_path,exist_ok=True)
 
+use_preview = True
+
 picam2 = Picamera2()
-# print("Sensor modes")
-# pprint(picam2.sensor_modes)
-# pprint(picam2.sensor_format) 
-picam2.start_preview(Preview.QTGL,x=1280,y=1, width = 640, height = 480)
+
+if use_preview:
+    picam2.start_preview(Preview.QTGL,x=1280,y=1, width = 640, height = 480)
 
 # not using raw formats as they are still not fully documented
 capture_config = picam2.create_still_configuration(
@@ -99,19 +105,37 @@ capture_config = picam2.create_still_configuration(
     buffer_count=1)
 # picam2.configure(capture_config)
 preview_config = picam2.create_preview_configuration(
-    main= {"format": "RGB888", "size": (2028,1520)},
+    main= {"format": "RGB888", "size": (4056,3040)},
     lores = {"format": "XBGR8888","size":(640,480)}, 
     # raw={"format": "SRGGB12", "size": (4056,3040)},
     display = "lores"
 )
 picam2.configure(preview_config)
-
 main_camera_stream_config = capture_config['main']
 
+# open the default image metadata and read in the settings as a sorted dict
+with open("camera_settings.txt") as f:
+    default_image_settings = f.read()
+default_image_settings = json.loads(default_image_settings)
+default_image_settings = sort_dict(default_image_settings)
+
+# apply the default settings to the current camera
+for key in default_image_settings:
+    try:
+        picam2.set_controls({key:default_image_settings[key]})
+    except:
+        print('FAIL to set camera setting')
+        print(key,default_image_settings[key])
+        
 picam2.start()
-time.sleep(0.5)
-picam2.title_fields = ["ColourTemperature","ColourGains"] # v"ExposureTime","AnalogueGain","DigitalGain",
-time.sleep(0.5)
+time.sleep(0.1)
+picam2.title_fields = ["ExposureTime","AnalogueGain"] # v"ExposureTime","AnalogueGain","DigitalGain",
+time.sleep(0.1)
+
+# Clean the output folder
+files = glob.glob(os.path.join(output_path, "*"))
+for f in files:
+    os.remove(f)
 
 print('capturing data')
 arrays, metadata = picam2.switch_mode_and_capture_arrays(capture_config, ["main","lores"])
@@ -119,7 +143,7 @@ camera_metadata = main_camera_stream_config
 metadata["ISO"] = round(100*metadata["AnalogueGain"])
 export_images(arrays,capture_config,metadata,camera_metadata,output_path)
 
-wait_time = 5
+wait_time = 10000
 print("waiting for", wait_time, '(s)')
 time.sleep(wait_time)
 
@@ -127,7 +151,7 @@ print('capturing data')
 arrays, metadata = picam2.switch_mode_and_capture_arrays(capture_config, ["main","lores"])
 camera_metadata = main_camera_stream_config
 metadata["ISO"] = round(100*metadata["AnalogueGain"])
-export_images(arrays,capture_config,metadata,camera_metadata,output_path)
+export_images(arrays,capture_config,metadata,camera_metadata,output_path,name_append="2")
 
 print('EOF')
 
